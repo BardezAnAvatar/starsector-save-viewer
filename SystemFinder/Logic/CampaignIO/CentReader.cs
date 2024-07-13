002ebf41d7d8eb8bd11959fd9298f4dce37ea2fd
Erik Frantz <bardez@bardez.net>
2024-07-13 00:00:36 -0500

    Add in a CCEnt reader

--- SystemFinder/Logic/CampaignIO/CentReader.cs
+++ SystemFinder/Logic/CampaignIO/CentReader.cs
@@ -3,10 +3,22 @@ using SystemFinder.Model.Data;
 
 namespace SystemFinder.Logic.CampaignIO
 {
-    public class CentReader
+    public class CentReader(MarketReader marketReader, MetadataReader meReader)
     {
         public void Read(XElement current, GalaxyData data)
         {
+            var market = current.Element("market");
+            var me = current.Element("me");
+
+            if (market is not null)
+            {
+                marketReader.Read(market, data);
+            }
+
+            if (me is not null)
+            {
+                meReader.Read(me, data);
+            }
         }
     }
 }
