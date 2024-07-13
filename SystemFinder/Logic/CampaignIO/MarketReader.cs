abaeb02c633b5f788ef1f4c19ba6bbf898cf7e53
Erik Frantz <bardez@bardez.net>
2024-07-12 23:52:21 -0500

    Add in a market reader

---
+++ SystemFinder/Logic/CampaignIO/MarketReader.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Linq;
+using SystemFinder.Model.Data;
+
+namespace SystemFinder.Logic.CampaignIO
+{
+    public class MarketReader
+    {
+        public void Read(XElement current, GalaxyData data)
+        {
+        }
+    }
+}
