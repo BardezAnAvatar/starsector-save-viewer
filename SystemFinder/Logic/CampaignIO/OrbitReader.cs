fbff875669f7fe4ef5cbdc0d0cbc45ab634c25cf
Erik Frantz <bardez@bardez.net>
2024-07-12 23:44:06 -0500

    Adding in an orbit reader

---
+++ SystemFinder/Logic/CampaignIO/OrbitReader.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Xml.Linq;
+using SystemFinder.Model.Data;
+
+namespace SystemFinder.Logic.CampaignIO
+{
+    public class OrbitReader(SavedObjectReader sReader, F_Reader fReader)
+    {
+        public void Read(XElement current, GalaxyData data)
+        {
+            var s = current.Element("s");
+            var f = current.Element("f");
+
+            if (s != null)
+            {
+                sReader.Read(s, data);
+            }
+
+            if (f != null)
+            {
+                fReader.Read(f, data);
+            }
+        }
+    }
+}
