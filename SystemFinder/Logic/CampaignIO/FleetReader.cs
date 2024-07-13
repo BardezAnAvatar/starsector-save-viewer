c126d1af85a0d83d561574156b0c9b2c0448b42c
Erik Frantz <bardez@bardez.net>
2024-07-13 00:10:22 -0500

    Populate Fleet reader

--- SystemFinder/Logic/CampaignIO/FleetReader.cs
+++ SystemFinder/Logic/CampaignIO/FleetReader.cs
@@ -3,10 +3,22 @@ using SystemFinder.Model.Data;
 
 namespace SystemFinder.Logic.CampaignIO
 {
-    public class FleetReader
+    public class FleetReader(dL_Reader dlReader, ScriptReader scReader)
     {
         public void Read(XElement current, GalaxyData data)
         {
+            var dL = current.Element("dL");
+            var sc = current.Element("sc");
+
+            if (dL is not null)
+            {
+                dlReader.Read(dL, data);
+            }
+
+            if (sc is not null)
+            {
+                scReader.Read(sc, data);
+            }
         }
     }
 }
