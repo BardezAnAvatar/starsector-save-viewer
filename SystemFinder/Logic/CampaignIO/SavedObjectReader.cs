e65a12ee5a542c730170f2ddff05d415e6b3afb8
Erik Frantz <bardez@bardez.net>
2024-07-12 23:27:01 -0500

    Reader for `./o/saved`

---
+++ SystemFinder/Logic/CampaignIO/SavedObjectReader.cs
@@ -0,0 +1,57 @@
+﻿using System.Xml.Linq;
+using SystemFinder.Model.Data;
+
+namespace SystemFinder.Logic.CampaignIO
+{
+    public class SavedObjectReader(LocationTokenReader locReader, CentReader centReader,
+        FleetReader fltReader, PlanetReader plntReader)
+    {
+        public void Read(XElement current, GalaxyData data)
+        {
+            //Get the whacky systems location:
+
+            var saved =
+                current
+                .Element("o")!
+                .Element("saved")!
+                ;
+
+            var locationTokens = saved.Elements("LocationToken");
+            var cents = saved.Elements("CCEnt");
+            var fleets = saved.Elements("Flt");
+            var planets = saved.Elements("Plnt");
+
+            if (locationTokens.Any())
+            {
+                foreach (var element in locationTokens)
+                {
+                    locReader.Read(element, data);
+                }
+            }
+
+            if (cents.Any())
+            {
+                foreach (var element in cents)
+                {
+                    centReader.Read(element, data);
+                }
+            }
+
+            if (fleets.Any())
+            {
+                foreach (var element in cents)
+                {
+                    fltReader.Read(element, data);
+                }
+            }
+
+            if (planets.Any())
+            {
+                foreach (var element in cents)
+                {
+                    plntReader.Read(element, data);
+                }
+            }
+        }
+    }
+}
