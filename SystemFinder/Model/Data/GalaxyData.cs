2f5b0c5baf435449326c6d0dd30f64b43b306faa
Erik Frantz <bardez@bardez.net>
2024-07-12 23:10:52 -0500

    Adding in a data dictionary housing

---
+++ SystemFinder/Model/Data/GalaxyData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemFinder.Model.Data
+{
+    public class GalaxyData
+    {
+        public Dictionary<string, Planet> Planets = new();
+        public Dictionary<string, StarSystem> StarSystems = new();
+        public Dictionary<string, Fleet> Fleets = new();
+        public Dictionary<string, Station> Stations = new();
+    }
+}
