c639e0f12ee5144e1e5765672930bcbb6b5f4f51
Erik Frantz <bardez@bardez.net>
2024-07-12 23:50:01 -0500

    Add in LocationToken reader

---
+++ SystemFinder/Logic/CampaignIO/LocationTokenReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using SystemFinder.Model;
+using SystemFinder.Model.Data;
+
+namespace SystemFinder.Logic.CampaignIO
+{
+    public class LocationTokenReader(OrbitReader orbitReader)
+    {
+        public void Read(XElement current, GalaxyData data)
+        {
+            var orbit = current.Element("orbit");
+
+            if (orbit is not null)
+            {
+                orbitReader.Read(orbit, data);
+            }
+        }
+    }
+}
