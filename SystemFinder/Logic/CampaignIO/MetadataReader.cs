e54c32ba57b2336fc0ca048654682013c73aa35e
Erik Frantz <bardez@bardez.net>
2024-07-13 00:13:02 -0500

    Adding in empty me reader

---
+++ SystemFinder/Logic/CampaignIO/MetadataReader.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Linq;
+using SystemFinder.Model.Data;
+
+namespace SystemFinder.Logic.CampaignIO
+{
+    public class MetadataReader
+    {
+        public void Read(XElement current, GalaxyData data)
+        {
+        }
+    }
+}
