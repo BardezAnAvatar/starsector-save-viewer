84293675b0405d1a92bb2be47012dbe92e2ccf4a
Erik Frantz <bardez@bardez.net>
2024-07-15 11:00:47 -0500

    Adding in a `con` reader

---
+++ SystemFinder/Logic/CampaignIO/Readers/ConReader.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml.Linq;
+using SystemFinder.Model.Data;
+
+namespace SystemFinder.Logic.CampaignIO.Readers
+{
+    public class ConReader(SstmReader sstmReader)
+    {
+        public void Read(XElement current, GalaxyData data)
+        {
+            var sstm = current
+                .Element("systems")
+                ?.Elements("Sstm");
+
+            if (sstm != null && sstm.Any())
+            {
+                foreach (var element in sstm)
+                {
+                    sstmReader.Read(element, data);
+                }
+            }
+        }
+    }
+}
