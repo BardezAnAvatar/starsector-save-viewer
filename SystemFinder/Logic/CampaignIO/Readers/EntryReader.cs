0e211b5bfdd8960ff7fd16df20a49312abf02b30
Erik Frantz <bardez@bardez.net>
2024-07-14 23:21:57 -0500

    Adding in an empty Entry reader

---
+++ SystemFinder/Logic/CampaignIO/Readers/EntryReader.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Linq;
+using SystemFinder.Model.Data;
+
+namespace SystemFinder.Logic.CampaignIO.Readers
+{
+    public class EntryReader
+    {
+        public void Read(XElement current, GalaxyData data)
+        {
+        }
+    }
+}
