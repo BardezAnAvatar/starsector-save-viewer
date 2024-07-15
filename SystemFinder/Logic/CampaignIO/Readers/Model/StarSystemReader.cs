55ca265da2da80648a9f6f7b88608eb022329ee4
Erik Frantz <bardez@bardez.net>
2024-07-15 11:26:16 -0500

    Adding in a reader for the Model that can be reused

---
+++ SystemFinder/Logic/CampaignIO/Readers/Model/StarSystemReader.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Linq;
+using SystemFinder.Model;
+using SystemFinder.Model.Data;
+
+namespace SystemFinder.Logic.CampaignIO.Readers.Model
+{
+    public class StarSystemReader
+    {
+        public void Read(XElement current, XAttribute uid, GalaxyData data)
+        {
+            var descriptiveName = current.Attribute("dN");
+            var shortName = current.Attribute("bN");
+            var systemType = current.Attribute("ty");
+
+            var system = new StarSystem
+            {
+                Ref = uid.Value,
+                Name = shortName!.Value,
+            };
+
+            data.StarSystems.Add(uid.Value, system);
+        }
+    }
+}
