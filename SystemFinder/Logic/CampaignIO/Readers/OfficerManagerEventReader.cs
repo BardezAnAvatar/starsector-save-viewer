3b3e868bca851f5c0a60c5e76d078141d5f7cacd
Erik Frantz <bardez@bardez.net>
2024-07-14 22:36:14 -0500

    Adding in an OfficerManagerEvent reader

---
+++ SystemFinder/Logic/CampaignIO/Readers/OfficerManagerEventReader.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml.Linq;
+using SystemFinder.Model.Data;
+
+namespace SystemFinder.Logic.CampaignIO.Readers
+{
+    public class OfficerManagerEventReader(PersonReader personReader)
+    {
+        public void Read(XElement current, GalaxyData data)
+        {
+            var available =
+                current
+                .Element("available")
+                ;
+
+            if (available is not null)
+            {
+                var officers = available.Elements("AvailableOfficer");
+
+                if (officers.Any())
+                {
+                    foreach (var element in officers)
+                    {
+                        personReader.Read(element, data);
+                    }
+                }
+            }
+        }
+    }
+}
