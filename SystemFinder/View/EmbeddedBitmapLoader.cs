0df59535c34b3ea9a191977d84ee3ebfbd248b9b
Erik Frantz <bardez@bardez.net>
2024-07-15 14:11:24 -0500

    Whitespace

--- SystemFinder/View/EmbeddedBitmapLoader.cs
+++ SystemFinder/View/EmbeddedBitmapLoader.cs
@@ -5,10 +5,10 @@ namespace SystemFinder.View
     public static class EmbeddedBitmapLoader
     {
         /// <summary>
-        /// Load a new icon bitmap from embedded resources.
-        /// For the BitmapImage, make sure you reference 
-        /// WindowsBase and PresentationCore, and import 
-        /// the System.Windows.Media.Imaging namespace. 
+        ///     Load a new icon bitmap from embedded resources.
+        ///     For the BitmapImage, make sure you reference 
+        ///     WindowsBase and PresentationCore, and import 
+        ///     the System.Windows.Media.Imaging namespace. 
         /// </summary>
         /// <remarks>https://thebuildingcoder.typepad.com/blog/2012/06/retrieve-embedded-resource-image.html</remarks>
         public static Image? ResourceImage(Assembly a, string imageName)
