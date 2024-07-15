27b816cc8123256744caf735da7b420363e6692e
Erik Frantz <bardez@bardez.net>
2024-07-15 11:03:25 -0500

    Removing fields that will be used elsewhere

--- SystemFinder/Model/StarSystem.cs
+++ SystemFinder/Model/StarSystem.cs
@@ -2,14 +2,10 @@
 
 namespace SystemFinder.Model
 {
-    internal class StarSystem
+    public class StarSystem
     {
         public string Ref { get; init; } = string.Empty;
 
-        public List<Planet> Planets { get; init; } = [];
-
         public string Name { get; init; } = string.Empty;
-
-        public Vector2 Position { get; init; }
     }
 }
