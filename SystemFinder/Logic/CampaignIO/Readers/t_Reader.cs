6e700450934b6e340cbd46019c9dc3cae7c7d71b
Erik Frantz <bardez@bardez.net>
2024-07-16 13:32:59 -0500

    Expanding the `t` reader to accomodate `orbit` and `where`

--- SystemFinder/Logic/CampaignIO/Readers/t_Reader.cs
+++ SystemFinder/Logic/CampaignIO/Readers/t_Reader.cs
@@ -4,15 +4,27 @@ using SystemFinder.Model.Data;
 
 namespace SystemFinder.Logic.CampaignIO.Readers
 {
-    public class t_Reader(IcL_Reader clReader) : It_Reader
+    public class t_Reader(IcL_Reader clReader, IOrbitReader orbitReader, IWhereReader whereReader) : It_Reader
     {
         public void Read(XElement current, GalaxyData data)
         {
             var cL = current.Element("cL");
+            var orbit = current.Element("orbit");
+            var where = current.Element("where");
 
             if (cL is not null)
             {
                 clReader.Read(cL, data);
+            }
+
+            if (orbit is not null)
+            {
+                orbitReader.Read(orbit, data);
+            }
+
+            if (where is not null)
+            {
+                whereReader.Read(where, data);
             }
         }
     }
