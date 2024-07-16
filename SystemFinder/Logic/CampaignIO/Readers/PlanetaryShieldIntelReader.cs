f39b83fb7c2e1f5522f7871a38a2a477267494fc
Erik Frantz <bardez@bardez.net>
2024-07-16 15:29:28 -0500

    Adding in a reader for `com.fs.starfarer.api.impl.campaign.intel.bar.events.PlanetaryShieldIntel` and its dependencies

---
+++ SystemFinder/Logic/CampaignIO/Readers/PlanetaryShieldIntelReader.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml.Linq;
+using SystemFinder.Logic.CampaignIO.Readers.Abstractions;
+using SystemFinder.Model.Data;
+
+namespace SystemFinder.Logic.CampaignIO.Readers
+{
+    public class PlanetaryShieldIntelReader(IEventReader eventReader, IPlanetReader planetReader)
+    {
+        public void Read(XElement current, GalaxyData data)
+        {
+            var @event = current.Element("event");
+            var planet = current.Element("planet");
+
+            if (@event is not null)
+            {
+                eventReader.Read(@event, data);
+            }
+
+            if (planet is not null)
+            {
+                planetReader.Read(planet, data);
+            }
+        }
+    }
+}
