0845ae48d1ea5a141840024a1fab27a65a2e126a
Erik Frantz <bardez@bardez.net>
2024-07-17 10:11:45 -0500

    Expand Planet to also read `orbit`

--- SystemFinder/Logic/CampaignIO/Readers/PlanetReader.cs
+++ SystemFinder/Logic/CampaignIO/Readers/PlanetReader.cs
@@ -6,17 +6,24 @@ using SystemFinder.Shared;
 
 namespace SystemFinder.Logic.CampaignIO.Readers
 {
-    public class PlanetReader(ILogger<PlanetReader> logger, Lazy<IMarketReader> marketReader) : IPlanetReader
+    public class PlanetReader(ILogger<PlanetReader> logger, Lazy<IMarketReader> marketReader,
+        IOrbitReader orbitReader) : IPlanetReader
     {
         public void Read(XElement current, GalaxyData data)
         {
             logger.Log(LogLevel.Debug, current.GetAbsoluteXPath());
 
             var market = current.Element("market");
+            var orbit = current.Element("orbit");
 
             if (market is not null)
             {
                 marketReader.Value.Read(market, data);
+            }
+
+            if (orbit is not null)
+            {
+                orbitReader.Read(orbit, data);
             }
         }
     }
