fea270fff711d0156d435fc50be2e4394a70ed1f
Erik Frantz <bardez@bardez.net>
2024-07-17 14:20:25 -0500

    Expanding `orbit` to `sP` child support

--- SystemFinder/Logic/CampaignIO/Readers/OrbitReader.cs
+++ SystemFinder/Logic/CampaignIO/Readers/OrbitReader.cs
@@ -6,7 +6,8 @@ using SystemFinder.Shared;
 
 namespace SystemFinder.Logic.CampaignIO.Readers
 {
-    public class OrbitReader(ILogger<OrbitReader> logger, Is_Reader sReader, If_Reader fReader) : IOrbitReader
+    public class OrbitReader(ILogger<OrbitReader> logger, Is_Reader sReader,
+        If_Reader fReader) : IOrbitReader
     {
         public void Read(XElement current, GalaxyData data)
         {
@@ -14,6 +15,7 @@ namespace SystemFinder.Logic.CampaignIO.Readers
 
             var f = current.Element("f");
             var s = current.Element("s");
+            var spOrbit = current.Element("sP")?.Element("orbit");
 
             if (f is not null)
             {
@@ -23,6 +25,11 @@ namespace SystemFinder.Logic.CampaignIO.Readers
             if (s is not null)
             {
                 sReader.Read(s, data);
+            }
+
+            if (spOrbit is not null)
+            {
+                Read(spOrbit, data);
             }
         }
     }
