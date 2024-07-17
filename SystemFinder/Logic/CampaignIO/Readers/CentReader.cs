106a4522be4293b8614f5bf4eadd414145a82082
Erik Frantz <bardez@bardez.net>
2024-07-17 10:32:33 -0500

    Expand `CCent` to `p` child support

--- SystemFinder/Logic/CampaignIO/Readers/CentReader.cs
+++ SystemFinder/Logic/CampaignIO/Readers/CentReader.cs
@@ -6,7 +6,8 @@ using SystemFinder.Shared;
 
 namespace SystemFinder.Logic.CampaignIO.Readers
 {
-    public class CentReader(ILogger<CentReader> logger, Lazy<IMarketReader> marketReader, IMetadataReader meReader)
+    public class CentReader(ILogger<CentReader> logger, Lazy<IMarketReader> marketReader, IMetadataReader meReader,
+        Ip_Reader pReader)
         : ICentReader
     {
         public void Read(XElement current, GalaxyData data)
@@ -15,6 +16,7 @@ namespace SystemFinder.Logic.CampaignIO.Readers
 
             var market = current.Element("market");
             var me = current.Element("me");
+            var p = current.Element("p");
 
             if (market is not null)
             {
@@ -24,6 +26,11 @@ namespace SystemFinder.Logic.CampaignIO.Readers
             if (me is not null)
             {
                 meReader.Read(me, data);
+            }
+
+            if (p is not null)
+            {
+                pReader.Read(p, data);
             }
         }
     }
