bf1ccf1b34b1c74a5d339003156837fd894b84e3
Erik Frantz <bardez@bardez.net>
2024-07-18 02:38:02 -0500

    Override ToString()

--- SystemFinder/Model/StarSystem.cs
+++ SystemFinder/Model/StarSystem.cs
@@ -5,5 +5,7 @@
         public string Id { get; init; } = string.Empty;
 
         public string Name { get; init; } = string.Empty;
+
+        public override string ToString() => $"{{ Name: {Name}; Id: {Id} }}";
     }
 }
