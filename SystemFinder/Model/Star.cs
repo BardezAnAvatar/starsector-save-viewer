031149d5064726707379d572cdf13b9d5768a86b
Erik Frantz <bardez@bardez.net>
2024-07-18 01:46:18 -0500

    Add in a Star class entity

---
+++ SystemFinder/Model/Star.cs
@@ -0,0 +1,11 @@
+﻿namespace SystemFinder.Model
+{
+    public class Star
+    {
+        public string Id { get; init; } = string.Empty;
+
+        public string StarSystemId { get; init; } = string.Empty;
+
+        public string Name { get; init; } = string.Empty;
+    }
+}
