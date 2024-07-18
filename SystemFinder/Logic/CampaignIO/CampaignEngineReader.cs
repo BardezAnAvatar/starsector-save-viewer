1aaa6c30edaf25cfcacd519479025fffaf989665
Erik Frantz <bardez@bardez.net>
2024-07-18 00:12:28 -0500

    Refactor the search to avoid all this over-engineered reader bullshit

--- SystemFinder/Logic/CampaignIO/CampaignEngineReader.cs
+++ SystemFinder/Logic/CampaignIO/CampaignEngineReader.cs
@@ -1,46 +1,49 @@
 ﻿using System.Xml.Linq;
 using Microsoft.Extensions.Logging;
-using Serilog.Core;
 using SystemFinder.Logic.CampaignIO.Abstractions;
-using SystemFinder.Logic.CampaignIO.Readers.Abstractions;
+using SystemFinder.Logic.CampaignIO.Readers.Abstractions.Model;
 using SystemFinder.Model.Data;
 
 namespace SystemFinder.Logic.CampaignIO
 {
-    public class CampaignEngineReader(ILogger<CampaignEngineReader> logger, Ie_Reader eReader,
-        IHyperspaceReader hyperspaceReader,
-        IStarSystemsReader starSystemsReader)
+    public class CampaignEngineReader(ILogger<CampaignEngineReader> logger, IStarSystemReader starSystemReader)
         : ICampaignEngineReader
     {
         public void Read(XDocument root, GalaxyData data)
         {
-            var campaign = root.Element("CampaignEngine")!;
+            FindSystems(root, data);
+        }
 
-            var hyperspace = campaign.Element("hyperspace");
-            var modE = campaign
-                .Element("modAndPluginData")
-                ?.Element("persistentData")
-                ?.Elements("e");
-            var starSystems = campaign.Element("starSystems");
+        public void FindSystems(XDocument root, GalaxyData data)
+        {
+            logger.Log(LogLevel.Debug, "Searching for `Sstm` Systems ...");
+            var sstm = root
+                .Descendants("Sstm")
+                ?.Where(e => e.Attribute("z") is not null);
 
-            if (hyperspace is not null)
+            logger.Log(LogLevel.Debug, "Searching for Non-`Sstm` Systems ...");
+            var filter = root
+                .Descendants()
+                ?.Where(d => d.Attribute("z") is not null && d.Attribute("cl")?.Value == "Sstm");
+
+            logger.Log(LogLevel.Debug, "Parsing `Sstm` Systems ...");
+            if (sstm is not null && sstm.Any())
             {
-                hyperspaceReader.Read(hyperspace, data);
-            }
-
-            if (modE is not null && modE.Any())
-            {
-                foreach (var element in modE)
+                foreach (var element in sstm)
                 {
-                    eReader.Read(element, data);
+                    var uid = element.Attribute("z")!;  //already established above (line 25)
+                    starSystemReader.Read(element, uid, data);
                 }
             }
 
-            if (starSystems is not null)
+            logger.Log(LogLevel.Debug, "Parsing Non-`Sstm` Systems ...");
+            if (filter is not null && filter.Any())
             {
-                var expectedCount = starSystems.Elements()?.Count();
-                logger.Log(LogLevel.Debug, $"Expected system count: {expectedCount}");
-                starSystemsReader.Read(starSystems, data);
+                foreach (var element in filter)
+                {
+                    var uid = element.Attribute("z")!;  //already established above (line 25)
+                    starSystemReader.Read(element, uid, data);
+                }
             }
         }
     }
