a841922b329635ea0779027d3bc0f470111b3136
Erik Frantz <bardez@bardez.net>
2024-07-18 01:26:52 -0500

    Enum identifying the indexes for images (for the read of XML and entry into tree view)

---
+++ SystemFinder/View/TreeViewIconIndexes.cs
@@ -0,0 +1,13 @@
+﻿namespace SystemFinder.View
+{
+    internal enum TreeViewIconIndexes
+    {
+        None = 0,
+        StarSystem = 1,
+        Star = 2,
+        Planet = 3,
+        GateInactive = 4,
+        GateActive = 5,
+        Station = 6,
+    }
+}
