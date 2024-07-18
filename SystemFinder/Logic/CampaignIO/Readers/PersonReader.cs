67893d621396ba3a80a982e4c5da462aca331d0b
Erik Frantz <bardez@bardez.net>
2024-07-17 23:08:10 -0500

    Adding `stats` child support to `person`

--- SystemFinder/Logic/CampaignIO/Readers/PersonReader.cs
+++ SystemFinder/Logic/CampaignIO/Readers/PersonReader.cs
@@ -6,17 +6,24 @@ using SystemFinder.Shared;
 
 namespace SystemFinder.Logic.CampaignIO.Readers
 {
-    public class PersonReader(ILogger<PersonReader> logger, Lazy<IMarketReader> marketReader) : IPersonReader
+    public class PersonReader(ILogger<PersonReader> logger, Lazy<IMarketReader> marketReader,
+        IStatsReader statsReader) : IPersonReader
     {
         public void Read(XElement current, GalaxyData data)
         {
             logger.Log(LogLevel.Debug, current.GetAbsoluteXPath());
 
             var market = current.Element("market");
+            var stats = current.Element("stats");
 
             if (market is not null)
             {
                 marketReader.Value.Read(market, data);
+            }
+
+            if (stats is not null)
+            {
+                statsReader.Read(stats, data);
             }
         }
     }
