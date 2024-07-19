7162b1076f5a36d67c7a0d518df202be94e9c0f8
Erik Frantz <bardez@bardez.net>
2024-07-19 00:33:41 -0500

    Adding in fields for Planet

--- SystemFinder/Model/Planet.cs
+++ SystemFinder/Model/Planet.cs
@@ -2,5 +2,20 @@
 {
     public class Planet
     {
+        public string Id { get; init; } = string.Empty;
+
+        public string StarSystemId { get; init; } = string.Empty;
+
+        public string Name { get; init; } = string.Empty;
+
+        public bool Colonized { get; init; } = false;
+
+        public bool Surveyed { get; init; } = false;
+
+        public int Size { get; init; } = 0;
+
+        public List<string> Conditions { get; init; } = [];
+
+        public override string ToString() => $"{{ Name: {Name}; Id: {Id}; StarSystemId: {StarSystemId}; Colonized: {Colonized} }}";
     }
 }
