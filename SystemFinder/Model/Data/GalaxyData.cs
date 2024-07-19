8f4afd8c1baaf1237b853427d8574fd8affe0983
Erik Frantz <bardez@bardez.net>
2024-07-18 22:34:31 -0500

    Add in Gate class

--- SystemFinder/Model/Data/GalaxyData.cs
+++ SystemFinder/Model/Data/GalaxyData.cs
@@ -2,10 +2,11 @@
 {
     public class GalaxyData
     {
-        public Dictionary<string, Planet> Planets = new();
-        public Dictionary<string, StarSystem> StarSystems = new();
-        public Dictionary<string, Star> Stars = new();
-        public Dictionary<string, Fleet> Fleets = new();
-        public Dictionary<string, Station> Stations = new();
+        public Dictionary<string, Planet> Planets { get; init; } = new();
+        public Dictionary<string, StarSystem> StarSystems { get; init; } = new();
+        public Dictionary<string, Star> Stars { get; init; } = new();
+        public Dictionary<string, Fleet> Fleets { get; init; } = new();
+        public Dictionary<string, Station> Stations { get; init; } = new();
+        public Dictionary<string, Gate> Gates { get; init; } = new();
     }
 }
---
+++ SystemFinder/Model/Gate.cs
@@ -0,0 +1,13 @@
+﻿namespace SystemFinder.Model
+{
+    public class Gate
+    {
+        public string Id { get; init; } = string.Empty;
+
+        public string StarSystemId { get; init; } = string.Empty;
+
+        public string Name { get; init; } = string.Empty;
+
+        public override string ToString() => $"{{ Name: {Name}; Id: {Id}; StarSystemId: {StarSystemId} }}";
+    }
+}
